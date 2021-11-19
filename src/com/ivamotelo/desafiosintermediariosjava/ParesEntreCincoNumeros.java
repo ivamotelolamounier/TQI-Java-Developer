8a8eb557caba6b2ba9465fa1ac53c58745ebd66b
ivamotelolamounier <ivamotelo@gmail.com>
2021-11-19 10:28:20 -0300

    TQI - Java Developer - Desafios em Java - Intermediários  I Commit inicial

---
+++ src/com/ivamotelo/desafiosintermediariosjava/ParesEntreCincoNumeros.java
@@ -0,0 +1,39 @@
+/**
+ * Básico - Princípios Básicos
+ * Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre
+ * esta informação.
+ *
+ * Entrada
+ * O arquivo de entrada contém 5 valores inteiros quaisquer.
+ *
+ * Saída
+ * Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+ *
+ * Exemplo de Entrada	Exemplo de Saída
+ * 7
+ * -5
+ * 6
+ * -4
+ * 12                   3 valores pares
+ */
+
+package com.ivamotelo.desafiosintermediariosjava;
+
+import java.util.Scanner;
+
+public class ParesEntreCincoNumeros {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int count = 0;
+
+        for(int i = 0; i < 5; i++) {
+            int n = scan.nextInt();
+
+            if (n % 2 == 0) {
+                count++;
+            }
+        }
+        System.out.printf("%d valores pares\n", count);
+    }
+}
